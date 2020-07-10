aef41dbd8f6e6f03987bba28f598892ad844d48a
hcmutemimi <hoaimiqng@gmail.com>
2020-07-10 20:37:14 +0700

    edit

--- Comestic/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Comestic/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -129,29 +129,7 @@ namespace Comestic.Areas.Identity.Pages.Account
                     }
 
                     return RedirectToAction("Index", "User", new { area = "Admin" });
-                    _logger.LogInformation("User created a new account with password.");
-
-                    //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    //code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    //var callbackUrl = Url.Page(
-                    //    "/Account/ConfirmEmail",
-                    //    pageHandler: null,
-                    //    values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
-                    //    protocol: Request.Scheme);
-
-                    //await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                    //    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-                    //if (_userManager.Options.SignIn.RequireConfirmedAccount)
-                    //{
-                    //    return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
-                    //}
-                    //else
-                    //{
-
-
-                    //}
-
+       
                 }
                 foreach (var error in result.Errors)
                 {
@@ -159,7 +137,6 @@ namespace Comestic.Areas.Identity.Pages.Account
                 }
             }
 
-            // If we got this far, something failed, redisplay form
             return Page();
         }
     }
